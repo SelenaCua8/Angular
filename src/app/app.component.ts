8df5910b253b84fa3be741666d548be76552514f
Selena <Selenac99@hotmail.com>
2023-09-20 13:44:11 -0300

    Fin de sección 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title: string ="hola mundo";
+
+
+}
