fffb31405e6d516f255d4ba2ee1803b6d38dcf0e
Selena <Selenac99@hotmail.com>
2023-09-26 13:24:09 -0300

    aprendiendo ngClass u ngFor

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface'
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
-    public heroNames: string [] = ['Goku', 'Goku1', 'Goku2', 'She Hulk', 'Thor'];
+   public characters: Character[] = [
+    {
+        name: 'Krillin',
+        power: 1000
+    },{
+        name: 'Goku',
+        power: 9500
+    },
+    {
+        name: 'Vegeta',
+        power: 7500,
+    }
+   ];
     
 }
