c6705c7011a1e20244db86bf7e06fc7d0080ba20
Selena <Selenac99@hotmail.com>
2023-09-26 14:56:27 -0300

    terminando seccion de agregar y sacar personajes dbz

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Character } from '../../interfaces/character.interface'
 
 @Component({
@@ -14,4 +14,15 @@ export class ListComponent {
     power: 10
   }]
 
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    //TODO: Emitir el ID del personaje
+    if ( !id) return;
+
+   this.onDelete.emit(id);
+
+}
 }
